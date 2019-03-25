6d378d3546c484b6249a9e92dfd81f65fff681c8
Pornjed Sakgitjarung <pornjed.sak@pantavanij.com>
2019-03-25 18:35:34 +0700

    update season display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>
+const SeasonDisplay = (props) => {
+    console.log(props.lat);
+    console.log(props.long);
+    return <div>Season Display {props.lat}, {props.long}</div>
 }
 
 export default SeasonDisplay;
